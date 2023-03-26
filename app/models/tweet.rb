b272b5558baff2754010d6820ca1dc6f20076507
tolaseadegbite <tolasekelvin@gmail.com>
2023-03-26 12:29:00 +0100

    Render replies count

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -9,6 +9,11 @@ class Tweet < ApplicationRecord
   has_many :retweeted_users, through: :retweets, source: :user
   has_many :views
   has_many :viewed_users, through: :views, source: :user
-  belongs_to :parent_tweet, inverse_of: :reply_tweets, foreign_key: :parent_tweet_id, class_name: "Tweet", optional: true
+  belongs_to  :parent_tweet, 
+              inverse_of: :reply_tweets, 
+              foreign_key: :parent_tweet_id, 
+              class_name: "Tweet", 
+              optional: true,
+              counter_cache: :reply_tweets_count
   has_many :reply_tweets, foreign_key: :parent_tweet_id, class_name: "Tweet"
 end
