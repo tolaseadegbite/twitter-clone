5b20eda074cc06123854004da32fede3bf03d3ac
tolaseadegbite <tolasekelvin@gmail.com>
2023-03-29 23:55:54 +0100

    Follow and unfollow user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     # before_action :authenticate_user!
 
     def show
+        redirect_to profile_path if params[:id].to_i == current_user.id
         @user ||= User.find(params[:id])
     end
 
