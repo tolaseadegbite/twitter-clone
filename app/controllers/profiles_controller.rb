db696bf244e5ebb62fcc9b7fef43940bda81b478
tolaseadegbite <tolasekelvin@gmail.com>
2023-03-31 10:18:02 +0100

    Unify profile and user pages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,11 +2,13 @@ class ProfilesController < ApplicationController
     before_action :authenticate_user!
 
     def show
-        
+        @user = current_user
+        render "users/show"
     end
 
     def update
-        if current_user.update(user_params)
+        @user = current_user
+        if @user.update(user_params)
             respond_to do |format|
                 # flash[:notice] = "Your profile has been updated"
                 format.html { redirect_to profile_path }
