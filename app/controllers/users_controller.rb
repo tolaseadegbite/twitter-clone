d0faa94dabac02bead0a41ef74fc125410982272
tolaseadegbite <tolasekelvin@gmail.com>
2023-03-31 11:21:26 +0100

    Render tweets on users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
-    # before_action :authenticate_user!
+    before_action :authenticate_user!
 
     def show
         redirect_to profile_path if params[:id].to_i == current_user.id
         @user ||= User.find(params[:id])
+        @tweet_presenters = @user.tweets.map do |tweet|
+            TweetPresenter.new(tweet: tweet, current_user: @user)
+        end
     end
-
 
 end
