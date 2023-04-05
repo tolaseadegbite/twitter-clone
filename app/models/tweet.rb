d631e9b2560b968204101b14c1e604272bf6c256
tolaseadegbite <tolasekelvin@gmail.com>
2023-04-05 22:50:10 +0100

    Create hashtags

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,5 @@
 class Tweet < ApplicationRecord
+  HASHTAG_REGEX = /(#\w+)/
   belongs_to :user
   validates :body, presence: true, length: { maximum: 280 }
   has_many :likes, dependent: :destroy
@@ -17,4 +18,15 @@ class Tweet < ApplicationRecord
               counter_cache: :reply_tweets_count
   has_many :reply_tweets, foreign_key: :parent_tweet_id, class_name: "Tweet"
   has_and_belongs_to_many :hashtags
+
+  before_save :parse_and_save_hashtags
+
+  def parse_and_save_hashtags
+    matches = body.scan(HASHTAG_REGEX)
+    return if matches.empty?
+
+    matches.flatten.each do |tag|
+      Hashtag.find_or_create_by(tag: tag.delete("#"))
+    end
+  end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -15,4 +15,31 @@ RSpec.describe Tweet, type: :model do
   it { should belong_to(:parent_tweet).with_foreign_key(:parent_tweet_id).class_name("Tweet").inverse_of(:reply_tweets).optional }
   it { should have_many(:reply_tweets).with_foreign_key(:parent_tweet_id).class_name("Tweet") }
   it { should have_and_belong_to_many(:hashtags) }
+
+  describe "saving hashtags" do
+    let(:user) { create(:user) }
+    context "when there are no hashtags in the body" do
+      it "does not create any hashtags" do
+        expect do
+          Tweet.create(user: user, body: "Mary had a little lamb")
+        end.not_to change { Hashtag.count }
+      end
+    end
+    context "when there are hashtags in the body" do
+      it "creates hashtags" do
+        expect do
+          Tweet.create(user: user, body: "Mary has a #little #lamb")
+        end.to change { Hashtag.count }.by(2)
+      end
+    end
+
+    context "when there are duplicate hashtags in the body" do
+      it "does not create extra hashtags if already in the database" do
+        Hashtag.create(tag: "little")
+        expect do
+          Tweet.create(user: user, body: "Mary has a #new #little sister")
+        end.to change { Hashtag.count }.by(1)
+      end
+    end
+  end
 end
