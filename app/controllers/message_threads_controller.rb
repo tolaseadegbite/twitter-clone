ee3f9cfb11b4cd8555e5b67bc9c58ac6a875a24a
tolaseadegbite <tolasekelvin@gmail.com>
2023-04-07 11:33:04 +0100

    Build a new message thread

--- app/controllers/message_threads_controller.rb
+++ app/controllers/message_threads_controller.rb
@@ -2,6 +2,8 @@ class MessageThreadsController < ApplicationController
     before_action :authenticate_user!
 
     def index
-        
+        if params[:user_id].present?
+            @new_message_thread_user = User.find(params[:user_id])
+        end
     end
 end
