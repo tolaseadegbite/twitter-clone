3281c606ea0b60d84b5f62fd1b824e7850f1aad3
tolaseadegbite <tolasekelvin@gmail.com>
2023-04-10 19:20:44 +0100

    Render messages when drilled into

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -12,10 +12,23 @@ export default class extends Controller {
       messageThread.addEventListener('click', (e) => {
         e.preventDefault();
 
+        this.messageThreadTargets.forEach((messageThread) => {
+          messageThread.classList.remove('active');
+        });
+
+        messageThread.classList.add('active');
+
         fetch(`/messages/${messageThread.dataset.messageThreadId}/messages`, { headers: this.headers })
           .then(response => response.text())
           .then(html => Turbo.renderStreamMessage(html));
       });
     });
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    if (urlParams.get('user_id') !== null) {
+      this.element.querySelector(`[data-messages-target-user-id="${urlParams.get('user_id')}"]`).click();
+      this.element.querySelector(`[data-messages-target-user-id="${urlParams.get('user_id')}"]`).classList.add('active')
+    }
   }
 }
