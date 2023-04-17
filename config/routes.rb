6cdb5969ceb1393073320045e7d782403b7da3f8
tolaseadegbite <tolasekelvin@gmail.com>
2023-04-17 01:31:25 +0100

    Render notifications page

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,6 @@ Rails.application.routes.draw do
   resources :users, only: :show do
     resources :followings, only: [ :create, :destroy]
   end
+
+  resources :notifications, only: :index
 end
--- db/migrate/20230313181927_remove_integer_from_tweets.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIntegerFromTweets < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :tweets, :integer, :string
-  end
-end
