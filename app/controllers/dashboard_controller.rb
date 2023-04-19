2c744f662bc2f932b0e65c0ef92e1974d8e04bd9
tolaseadegbite <tolasekelvin@gmail.com>
2023-04-19 01:56:12 +0100

    Render tweet activities

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,13 +2,8 @@ class DashboardController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        # @tweets = Tweet.includes(:liked_users, :bookmarked_users, :retweeted_users, :likes, :retweets).order(created_at: :desc).map do |tweet| 
-        #     TweetPresenter.new(tweet: tweet, current_user: current_user)
-        # end
-
-        following_users = current_user.following_users
-        @tweets = Tweet.includes(:liked_users, :bookmarked_users, :retweeted_users, :likes, :retweets).where(user: following_users).order(created_at: :desc).map do |tweet| 
-            TweetPresenter.new(tweet: tweet, current_user: current_user)
+        @tweet_activities = current_user.tweet_activities.order(created_at: :desc).map do |tweet_activity| 
+            TweetPresenter.new(tweet: tweet_activity.tweet, current_user: current_user)
         end
     end
 end
