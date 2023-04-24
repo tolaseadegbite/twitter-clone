e3606730d1c26231636fca66a23f315d7f40b30f
tolaseadegbite <tolasekelvin@gmail.com>
2023-04-24 22:39:16 +0100

    Install and setup Kaminari

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,16 @@ class DashboardController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        @tweet_activities = current_user.tweet_activities.order(created_at: :desc).map do |tweet_activity| 
-            TweetPresenter.new(tweet: tweet_activity.tweet, current_user: current_user, tweet_activity: tweet_activity)
-        end
+        @tweet_activities = current_user.tweet_activities.order(created_at: :desc).page(params[:page]).per(5)
+
+        @tweet_activities_data = {
+            tweet_activities: @tweet_activities.map do |tweet_activity| 
+                TweetPresenter.new(tweet: tweet_activity.tweet, current_user: current_user, tweet_activity: tweet_activity)
+            end,
+            page: @tweet_activities.current_page,
+            next_page: @tweet_activities.next_page,
+            last_page: @tweet_activities.last_page?,
+            total_pages: @tweet_activities.total_pages
+        }
     end
 end
